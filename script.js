0e78422f83d16f2d4b09032a96e99945ec0a9b3c
Nathan Wright <nathanw1214@gmail.com>
2021-08-23 09:12:48 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var cookiesDiv = document.querySelector("#cookies") // Target Cookies Div
+
+function accept(){ // Function when Accept Btn is Pressed
+    cookiesDiv.remove()
+}
+
+var newHigh = document.querySelector(".high")
+var newLow = document.querySelector(".low")
+
+function chngTemp(element){
+
+}
